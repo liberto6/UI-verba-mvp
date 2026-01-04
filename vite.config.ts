5d2b764724ea7259200dcb7ed0f055ddad1acbb7
Pepe Da-Rosa <pepdamar@alum.us.es>
2026-01-04 20:02:10 +0100

    Prueba 4

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0', // Listen on all interfaces (required for RunPod/Docker)
-    port: 8000,
+    port: 5173,
     strictPort: true,
   },
 })
